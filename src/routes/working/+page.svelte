d2e2c351f38e009aea21bd4518dce57c1196bfd4
Tim Guggenmos <tim.guggenmos@me.com>
2024-11-12 17:29:55 +0100

    added csr false duplicate

---
+++ src/routes/working/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from "$app/stores"
+</script>
+
+<!-- MARK: Cover -->
+<div class="relative h-[60vh] min-h-[22rem] bg-neutral-800 bg-cover">
+    <enhanced:img src="$images/cover.jpeg?w=500;1000;1500;2000" alt="" class="h-full w-full object-cover" />
+</div>
