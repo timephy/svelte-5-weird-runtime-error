adb295deff163979b2d16d083f92756771c05b28
Tim Guggenmos <tim.guggenmos@me.com>
2024-11-12 17:34:10 +0100

    clean

--- src/routes/working/+page.svelte
+++ src/routes/working/+page.svelte
@@ -1,8 +1,1 @@
-<script lang="ts">
-    import { page } from "$app/stores"
-</script>
-
-<!-- MARK: Cover -->
-<div class="relative h-[60vh] min-h-[22rem] bg-neutral-800 bg-cover">
-    <enhanced:img src="$images/cover.jpeg?w=500;1000;1500;2000" alt="" class="h-full w-full object-cover" />
-</div>
+<enhanced:img src="$images/cover.jpeg?w=500;1000;1500;2000" alt="" />
